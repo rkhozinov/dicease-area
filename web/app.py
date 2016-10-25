229270a37af9bc6638dc83101fe23fc3dbb51366
Ruslan Khozinov <rkhozinov@mirantis.com>
2016-10-25 20:14:57 +0300

    version 0.1
     - base logic
     - base design

--- web/app.py
+++ web/app.py
@@ -1,99 +1,151 @@
 # app.py
+from sys import path
+from os.path import dirname as dir
+
+path.append(dir(path[0]))
 
 import logging
 
-import requests
-from app_config import BaseConfig
+from config import Config
 from flask import Flask
-from flask import request, render_template, redirect, url_for
+from flask import request, render_template, redirect, url_for, flash
 from flask_sqlalchemy import SQLAlchemy
-
-from functools import wraps
+import requests
 
 app = Flask(__name__)
-app.config.from_object(BaseConfig)
+
+app.config.from_object(Config)
+
 db = SQLAlchemy(app)
+db.init_app(app)
+
+from models import *
+
+with app.test_request_context():
+    db.create_all()
+
 console = logging.StreamHandler()
 log = logging.getLogger("app")
 log.addHandler(console)
 log.setLevel(logging.DEBUG)
 
-from models import *
+navigation = ['district', 'hospital', 'disease']
 
-def templated(template=None):
-    def decorator(f):
-        @wraps(f)
-        def decorated_function(*args, **kwargs):
-            template_name = template
-            if template_name is None:
-                template_name = request.endpoint \
-                                    .replace('.', '/') + '.html'
-            ctx = f(*args, **kwargs)
-            if ctx is None:
-                ctx = {}
-            elif not isinstance(ctx, dict):
-                return ctx
-            return render_template(template_name, **ctx)
-
-        return decorated_function
-
-    return decorator
 
 @app.route('/', methods=['GET', 'POST'])
 def index():
-    return render_template('index.html', feeders=_get_all_feeders())
+    return render_template('index.html', navigation=navigation)
 
 
-@app.route('/feeder', methods=['GET', 'POST'])
-@app.route('/feeder/add', methods=['GET', 'POST'])
-def feeder():
+@app.route('/district', methods=['GET', 'POST'])
+def district():
     if request.method == 'POST':
         log.debug(request.form)
-        feeder_type = request.form.get('type')
-        address = request.form.get('address')
         name = request.form.get('name')
-        port = request.form.get('port')
-        db.session.add(Feeder(type=feeder_type, name=name, address=address, port=port))
-        db.session.commit()
-    return render_template('feeder.html', feeders=_get_all_feeders())
+        coordinates = request.form.get('coordinates')
+
+        district = District.query.filter_by(name=name).first()
+        if name and not district:
+            db.session.add(District(name=name, coordinates=coordinates))
+            db.session.commit()
+        return redirect(url_for('district'))
+
+    return render_template('district.html', districts=_get_all_districts(),
+                           navigation=navigation)
 
 
-@app.route('/feeder/delete_last', methods=['POST'])
-def feeder_delete_last():
+@app.route('/district/delete', methods=['POST'])
+def district_delete():
+    if request.method == 'POST':
+        district_id = int(request.form.get('district_id'))
+        log.debug(request.form)
+        district = District.query.filter_by(id=district_id).first()
+
+        log.debug(district)
+
+        if district and not district.hospitals.count():
+            db.session.delete(district)
+            db.session.commit()
+
+    return redirect(url_for('district'))
+
+
+@app.route('/disease', methods=['GET', 'POST'])
+def disease():
     if request.method == 'POST':
         log.debug(request.form)
-        db.session.delete(Feeder.query.order_by(Feeder.id.desc()).first())
-        db.session.commit()
+        name = request.form.get('name')
+        if name:
+            db.session.add(Disease(name=name))
+            db.session.commit()
+    return render_template('disease.html', diseases=_get_all_diseases(),
+                           navigation=navigation)
 
-    return redirect(url_for('index'))
 
-
-@app.route('/feeder/move', methods=['POST'])
-def feeder_move():
+@app.route('/disease/delete', methods=['POST'])
+def disease_delete():
     if request.method == 'POST':
-        feeder_name = request.form.get('feeder_name')
-
-        direction = request.form.get('direction')
+        disease_id = int(request.form.get('disease_id'))
         log.debug(request.form)
+        disease = Disease.query.filter_by(id=disease_id).first()
 
-        feeder = Feeder.query.filter_by(name=feeder_name).first()
-        log.debug(feeder)
-        if not feeder:
-            redirect(url_for('index'))
-        r = requests.get("http://{address}:{port}/?{direction}".format(address=feeder.address,
-                                                                       port=feeder.port,
-                                                                       direction=direction))
-        response_data= r.text
-        response_status = r.status_code
-        content_type = r.headers['content-type']
+        if disease:
+            db.session.delete(disease)
+            db.session.commit()
 
-        log.debug(response_data)
-        log.debug(response_status)
+    return redirect(url_for('disease'))
 
-        return redirect(url_for('index'))
 
-def _get_all_feeders():
-    return Feeder.query.order_by(Feeder.address).all()
+@app.route('/hospital', methods=['GET', 'POST'])
+def hospital():
+    if request.method == 'POST':
+        log.debug(request.form)
+        name = request.form.get('name')
+        address = request.form.get('address')
+        phone = request.form.get('phone')
+        coordinates = request.form.get('coordinates')
+        district_id = request.form.get('district_id')
+
+        district = District.query.filter_by(id=district_id).first() if district_id else None
+
+        if name and district:
+            db.session.add(Hospital(name=name, address=address, phone=phone,
+                                    coordinates=coordinates, district=district))
+            db.session.commit()
+
+    return render_template('hospital.html', hospitals=_get_all_hospitals(),
+                           districts=_get_all_districts(),
+                           navigation=navigation)
+
+
+@app.route('/hospital/delete', methods=['POST'])
+def hospital_delete():
+    if request.method == 'POST':
+        hospital_id = int(request.form.get('hospital_id'))
+        log.debug(request.form)
+        hospital = Hospital.query.filter_by(id=hospital_id).first()
+        if hospital:
+            db.session.delete(hospital)
+            db.session.commit()
+
+    return redirect(url_for('hospital'))
+
+
+def _get_all_districts():
+    return District.query.order_by(District.name).all()
+
+
+def _get_all_diseases():
+    return Disease.query.order_by(Disease.name).all()
+
+
+def _get_all_hospitals():
+    return Hospital.query.order_by(Hospital.name).all()
+
+
+def _get_coordinates(place):
+    pass
+
 
 if __name__ == '__main__':
     app.run()
